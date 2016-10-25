c2dfb8bd7fc2258cdd4631b10fa3f180d0adf169
luffycodes <luffycodes@users.noreply.github.com>
2016-10-25 12:26:11 +0530

    removed comments
    removed comments

--- stitcher.cpp
+++ stitcher.cpp
@@ -12,32 +12,15 @@ vector< vector<string> > gesture[13];
 int main()
 {
   initialise_similarity_matrix();
-  
-//   string str1, str2;
-//   cout << "Enter first string \n";
-//   cin >> str1;
-//   cout << "Enter second string \n";
-//   cin >> str2;
-  
-//   cout << GestureSeqSimilarity(str1,str2);	
-//   string fileName1;
-//   cout << "Enter the first file name\n";
-//   cin >> fileName1;
-//  string file_throw = "P1_1_8_p19";
-//  string file_had_enough = "P1_1_9_p21";
   string allFiles = "gestureFileNames";
   ifstream allGesturesFile(allFiles.c_str());
   string fileName;
   
   while(getline(allGesturesFile,fileName))
   {
-    //if(findGestureNo(fileName) == 8 || findGestureNo(fileName) == 9)
       cout << fileName << "\n";
       readData(fileName);
   }
-  
-//   readData(file_throw);
-//   readData(file_had_enough);
   
   int gestureNoThrow = 8;//findGestureNo(file_throw);
   int gestureNoHadEnough = 9;//= findGestureNo(file_had_enough);
@@ -47,17 +30,7 @@ int main()
   
   int i,j;
   
-  string temp;/* = "diff_8_9";
-  ofstream diff_8_9(temp.c_str());
-  
-  temp = "same_8_8";
-  ofstream same_8_8(temp.c_str());
-  
-  temp = "same_9_9";
-  ofstream same_9_9(temp.c_str());
-  
-  temp = "gesture_9";
-  ofstream gesture_9(temp.c_str());*/
+  string temp;
   
   for(i=1;i<=12;i++)
   {  
@@ -88,53 +61,6 @@ int main()
       }
     }
   }
-      
-  
-//   for(i=1;i<throwGestureSize;i++)
-//   {
-//       for(j=1;j<hadEnoughGestureSize;j++)
-//       {
-// 	//cout << "Similarity between differents \n";
-// 	//cout << gesture[gestureNoThrow][i][10] << " and " << gesture[gestureNoHadEnough][j][10] << "is \n";
-// 	diff_8_9 << GestureSeqSimilarity(gesture[gestureNoThrow][i] , gesture[gestureNoHadEnough][j]) << "\n";
-// 	//cout << "\n";
-//       }      
-//   }
-  /*cout << "\n\n\n Now for same";
-  cout << throwGestureSize << " " << hadEnoughGestureSize << "\n";
-  
-  for(i=1;i<throwGestureSize;i++)
-  {
-      for(j=i+1;j<throwGestureSize;j++)
-      {
-	//cout << "Similarity between Same's\n";
-	//cout << gesture[gestureNoThrow][i][10] << " and " << gesture[gestureNoThrow][j][10] << "is \n";
-	same_8_8 << GestureSeqSimilarity(gesture[gestureNoThrow][i] , gesture[gestureNoThrow][j])<< "\n";
-	//cout << "\n";
-      }
-  }
-  cout << "\n\n\n Now for same";
-  
-  for(i=1;i<hadEnoughGestureSize;i++)
-  {
-      for(j=i+1;j<hadEnoughGestureSize;j++)
-      {
-	//cout << "Similarity between Same's\n";
-	//cout << gesture[gestureNoHadEnough][i][10] << " and " << gesture[gestureNoHadEnough][j][10] << "is \n";
-	same_9_9 << GestureSeqSimilarity(gesture[gestureNoHadEnough][i] , gesture[gestureNoHadEnough][j])<< " ";
-	//cout << "\n";
-      }
-  }*/  
-  
-  /*for(int i =0; i <gesture[9].size();i++)
-  {
-    gesture_9 << "---------------------------------------------------------------------------\n";
-    gesture_9 << "\nGesture No. " << i << "\n";
-    for(int j =0; j<20;j++)
-    {
-      gesture_9 << "Joint ID:" << j << "\tCube Sequence ->" << gesture[9][i][j] << "\n";      
-    }    
-  } */ 
   
   return 0;
 }
